fdc9a4114fac01eb298e222210e917c375669ba8
Hamzah Hameed <hamzahthegreat@yahoo.com>
2020-08-26 15:03:41 +0300

    Updates

---
+++ precache-manifest.51e1f0d26b00b2ee914d43468337bfe7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a5af4185b57223cbab6d700d34dee5",
+    "url": "/imagerecognition-frontend/index.html"
+  },
+  {
+    "revision": "7b83db2e0200c9c445a5",
+    "url": "/imagerecognition-frontend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2b3d1bbfcf25b309dd7b",
+    "url": "/imagerecognition-frontend/static/css/main.155f43c3.chunk.css"
+  },
+  {
+    "revision": "7b83db2e0200c9c445a5",
+    "url": "/imagerecognition-frontend/static/js/2.dcf4350b.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/imagerecognition-frontend/static/js/2.dcf4350b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3d1bbfcf25b309dd7b",
+    "url": "/imagerecognition-frontend/static/js/main.260d1b53.chunk.js"
+  },
+  {
+    "revision": "fb5ad22ee4ef497dd198",
+    "url": "/imagerecognition-frontend/static/js/runtime-main.02322c73.js"
+  }
+]);
